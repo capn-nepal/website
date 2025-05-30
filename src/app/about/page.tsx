9ba1142f7db286462f4ba3a00a58c836ad5eb96a
Shreeyash Shrestha <shreeyash.shrestha@togglecorp.com>
2025-05-30 16:20:02 +0545

    Add pop up component and enhance routing

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,33 +1,5 @@
-import Banner from '#components/Banner';
-import Divider from '#components/Divider';
-import Page from '#components/Page';
-import AboutUsImage from '#public/aboutUsImage.jpg';
+import { redirect } from 'next/navigation';
 
-import Goals from './Goals';
-import Values from './Values';
-import VisionMission from './VisionMission';
-
-import styles from './page.module.css';
-
-export default function About() {
-    return (
-        <Page>
-            <Banner
-                // NOTE: We need to replace with the real image as mentioned in figma
-                bannerImageSrc={AboutUsImage}
-                eyebrowHeading="Our Approach"
-                heading={(
-                    <>
-                        Empowering Change Through
-                        <br />
-                        <span>Gender-Equal Citizenship</span>
-                    </>
-                )}
-            />
-            <VisionMission />
-            <Divider />
-            <Goals />
-            <Values />
-        </Page>
-    );
+export default function AboutRedirect() {
+    redirect('/about/approach');
 }
