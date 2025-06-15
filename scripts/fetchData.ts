66662bb939f0ae0217199d9ac6df6844d71eefa5
Aditya Khatri <adityakhatri47@gmail.com>
2025-06-15 15:02:14 +0545

    Connect backend

--- scripts/fetchData.ts
+++ scripts/fetchData.ts
@@ -1,27 +1,36 @@
+import { GraphQLClient, gql } from 'graphql-request';
 import fs from 'fs';
 import path from 'path';
 
-const dataDir = path.join(__dirname, '../data');
+const datadir = path.join(__dirname, '../data');
+const GRAPHQL_ENDPOINT = process.env.APP_GRAPHQL_ENDPOINT || 'http://localhost:8000/graphql/';
+
+
+const client = new GraphQLClient(GRAPHQL_ENDPOINT);
+
+const query = gql`
+query Events {
+    events {
+        results {
+            startDate
+            name
+            location
+            id
+            endDate
+            description
+        }
+        totalCount
+    }
+}
+`;
+
 
 async function fetchAndWriteData() {
-    /*
-    const query = `{
-        events {
-            id
-            title
-            date
-            description
-            eventDescription
-        }
-    }`;
+    const data = await client.request(query);
 
-    const data = await request('https://your-api/graphql', query);
-    */
-    const data = { random: 12 };
-
-    // Ensure the `data` directory exists
-    if (!fs.existsSync(dataDir)) {
-        fs.mkdirSync(dataDir, { recursive: true });
+    // ensure the `data` directory exists
+    if (!fs.existsSync(datadir)) {
+        fs.mkdirSync(datadir, { recursive: true });
     }
     fs.writeFileSync(path.join(__dirname, '../data/staticData.json'), JSON.stringify(data, null, 2));
 }
