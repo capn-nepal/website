ada4ff6244c8829154b21f71b67687317bddf593
Aditya Khatri <adityakhatri47@gmail.com>
2025-07-01 10:21:47 +0545

    fix(chore): deploy to github

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,17 @@
-const withExportImages = require('next-export-optimize-images');
 import type { NextConfig } from 'next';
 
-// const repo = 'website';
+const repo = 'website';
+const pipelineType = process.env.PIPELINE_TYPE;
 
-const nextConfig: NextConfig = withExportImages({
+const nextConfig: NextConfig = {
     output: 'export',
     trailingSlash: true, // optional, depends on your server/CDN setup
-    // basePath: `/${repo}`,
-    // assetPrefix: `/${repo}/`,
-    images: {
-        domains: ['https://capn-minio-2.k8s.dev.togglecorp.com/capn-media/media/',],
-        deviceSizes: [640, 960, 1280, 1600, 1920],
-    },
-});
+    basePath: pipelineType === 'cd' ? `/${repo}` : undefined,
+    assetPrefix: pipelineType === 'cd' ? `/${repo}/` : undefined,
+    //images: {
+    //    domains: ['https://capn-minio-2.k8s.dev.togglecorp.com/capn-media/media/',],
+    //    deviceSizes: [640, 960, 1280, 1600, 1920],
+    //},
+};
 
 export default nextConfig;
